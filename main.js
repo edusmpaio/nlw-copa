eb67fb3eb50ea0fd63a77d4fd70c287cc80a2d00
Eduardo Sampaio <edupcsampaio@gmail.com>
2022-11-03 11:38:43 -0300

    refactoring app innerHTML

--- main.js
+++ main.js
@@ -26,29 +26,22 @@ function createCard(date, day, games) {
   `;
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="NLW Copa Calendario" />
-  </header>
-
-  <main class="cards">
-    ${createCard(
-      "24/11",
-      "quinta",
-      createGame("switzerland", "07:00", "cameroon") +
-        createGame("brazil", "16:00", "serbia")
-    )}
-    ${createCard(
-      "28/11",
-      "segunda",
-      createGame("cameroon", "07:00", "serbia") +
-        createGame("brazil", "13:00", "switzerland")
-    )}
-    ${createCard(
-      "02/12",
-      "sexta",
-      createGame("cameroon", "16:00", "brazil") +
-        createGame("serbia", "16:00", "switzerland")
-    )}
-  </main>
-`;
+document.querySelector(".cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("brazil", "13:00", "switzerland")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("cameroon", "16:00", "brazil") +
+      createGame("serbia", "16:00", "switzerland")
+  );
