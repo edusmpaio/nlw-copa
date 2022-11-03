337c784a91fde3bc0ba7cb4a0ed6b953c4830d84
Eduardo Sampaio <edupcsampaio@gmail.com>
2022-11-03 10:39:59 -0300

    add javscript

---
+++ main.js
@@ -0,0 +1,25 @@
+document.querySelector("#app").innerHTML = `
+  <header>
+    <img src="./assets/logo.svg" alt="NLW Copa Calendario" />
+  </header>
+
+  <main class="cards">
+    <div class="card">
+      <h2>24/11 <span>quinta</span></h2>
+
+      <ul>
+        <li>
+          <img
+            src="./assets/flags/brazil-flag.svg"
+            alt="Bandeira do Brasil"
+          />
+          <strong>16:00</strong>
+          <img
+            src="./assets/flags/serbia-flag.svg"
+            alt="Bandeira da Sérvia"
+          />
+        </li>
+      </ul>
+    </div>
+  </main>
+`;
