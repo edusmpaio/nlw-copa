6245845b300830335daccbee81e9a11d9c6f9411
Eduardo Sampaio <edupcsampaio@gmail.com>
2022-11-03 12:01:04 -0300

    adding animations

--- main.js
+++ main.js
@@ -14,9 +14,13 @@ function createGame(country1, hour, country2) {
   `;
 }
 
+let delay = -0.3;
+
 function createCard(date, day, games) {
+  delay += 0.3;
+
   return `
-    <div class="card">
+    <div class="card" style="animation-delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
 
       <ul>
