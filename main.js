ecf4ae25ebd6dcc11be061167168a94c5ea07a96
Eduardo Sampaio <edupcsampaio@gmail.com>
2022-11-03 11:15:16 -0300

    Aula 03 finalizada

--- main.js
+++ main.js
@@ -1,25 +1,54 @@
+function createGame(country1, hour, country2) {
+  return `
+    <li>
+      <img
+        src="./assets/flags/${country1}-flag.svg"
+        alt="Bandeira ${country1}"
+      />
+      <strong>${hour}</strong>
+      <img
+        src="./assets/flags/${country2}-flag.svg"
+        alt="Bandeira ${country2}"
+      />
+    </li>
+  `;
+}
+
+function createCard(date, day, games) {
+  return `
+    <div class="card">
+      <h2>${date} <span>${day}</span></h2>
+
+      <ul>
+        ${games}
+      </ul>
+    </div>
+  `;
+}
+
 document.querySelector("#app").innerHTML = `
   <header>
     <img src="./assets/logo.svg" alt="NLW Copa Calendario" />
   </header>
 
   <main class="cards">
-    <div class="card">
-      <h2>24/11 <span>quinta</span></h2>
-
-      <ul>
-        <li>
-          <img
-            src="./assets/flags/brazil-flag.svg"
-            alt="Bandeira do Brasil"
-          />
-          <strong>16:00</strong>
-          <img
-            src="./assets/flags/serbia-flag.svg"
-            alt="Bandeira da Sérvia"
-          />
-        </li>
-      </ul>
-    </div>
+    ${createCard(
+      "24/11",
+      "quinta",
+      createGame("switzerland", "07:00", "cameroon") +
+        createGame("brazil", "16:00", "serbia")
+    )}
+    ${createCard(
+      "28/11",
+      "segunda",
+      createGame("cameroon", "07:00", "serbia") +
+        createGame("brazil", "13:00", "switzerland")
+    )}
+    ${createCard(
+      "02/12",
+      "sexta",
+      createGame("cameroon", "16:00", "brazil") +
+        createGame("serbia", "16:00", "switzerland")
+    )}
   </main>
 `;
